d529507b7ab56896fa2de4157100470ff3b644a9
alvinnanxiongwang <alvinwang1026@gmail.com>
2022-07-07 15:18:15 -0700

    added marker

--- client/app/src/components/SearchAutocomplete.vue
+++ client/app/src/components/SearchAutocomplete.vue
@@ -22,11 +22,18 @@
   <GMapMap
       :center="center"
       :zoom="4"
+      ref="myMapRef"
       map-type-id="terrain"
       style="width: 80vw; height: 700px"
   >
     <GMapPolygon
         :paths="paths"
+        ref="myPolyRef"
+    />
+    <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
     />
   </GMapMap>
 </template>
@@ -34,6 +41,8 @@
 <script>
 import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
+import axios from "axios";
+import {ref} from "vue";
 
 export default {
   name: 'SearchAutocomplete',
@@ -44,15 +53,16 @@ export default {
   }
 `, { match: ''});
 
-
-
-
+    const myMapRef = ref();
+    const myPolyRef = ref();
 
     return {
       result,
       loading,
       error,
       refetch,
+      myMapRef,
+      myPolyRef
     }
   },
   props: {
@@ -66,12 +76,11 @@ export default {
     return {
       search: '',
       isOpen: false,
-      center: {lat: 25.774, lng: -80.19},
-      paths: [
-        { lat: 25.774, lng: -80.19 },
-        { lat: 18.466, lng: -66.118 },
-        { lat: 32.321, lng: -64.757 },
-      ],
+      center: {lat: 39.8283, lng: -98.5795},
+      //center: {lat: 25.774, lng: -80.19},
+      //center: {lat: 36, lng: -118},
+      paths: [],
+      markers: []
     };
   },
   methods: {
@@ -79,11 +88,54 @@ export default {
       this.refetch({match: this.search});
       this.isOpen = true;
     },
+
     onClickLocation(location) {
       this.search = location
-    }
+
+      let apiPath = "https://nominatim.openstreetmap.org/search.php";
+
+      let params = {
+        q: location,
+        polygon_geojson: 1,
+        format: "json"
+      };
+
+      axios.get(apiPath, { params: params }  ).then( (response) => {
+        this.myMapRef.$mapPromise.then(mapObject => {
+          mapObject.setCenter({
+            lat: Number(response.data[0].lat),
+            lng: Number(response.data[0].lon),
+          });
+          mapObject.setZoom(6)
+
+        //this.paths = response.data[0].geojson.coordinates
+        //console.log("paths", this.paths)
+        console.log("polygon", this.myPolyRef)
+        console.log('geo',response.data[0].geojson)
+        },
+
+        //$refs.myPolyRef
+        this.markers = [
+          {
+            position: {
+              lat: Number(response.data[0].lat), lng: Number(response.data[0].lon)
+            },
+          }
+        ],
+        this.paths = response.data[0].geojson.coordinates[0],
+        console.log('path', this.paths)
+        // this.myPolyRef.$mapPromise.then(() => {
+        //   this.paths = [
+        //     { lat: 32, lng: 33.3629593 },
+        //     { lat: 12.466, lng: -66.118 },
+        //     { lat: 80.321, lng: -64.757 },
+        //   ]
+        // }),
+        )
+      })
+    },
   },
-};
+}
 </script>
 
 <style>
